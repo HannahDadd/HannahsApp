1638f28257d803ad03a86e596211f3eb0a5f872e
Kyle H <kyle.henderson@bbc.co.uk>
2025-05-14 09:15:17 +0100

    Initial Commit

---
+++ HannahsApp/HannahsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HannahsAppApp.swift
+//  HannahsApp
+//
+//  Created by Kyle O Henderson on 14/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HannahsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
